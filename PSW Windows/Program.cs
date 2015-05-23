354f40dbbe16d0660ccffebd6c02b2bfe11d3a82
Benji <benji3.141@gmail.com>
2015-05-23 18:08:06 -0400

    PSW Pre-Alpha 0.1.0
    First version

---
+++ PSW Windows/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Benji;
+using Benji.PSW;
+using Benji.PSW.Windows;
+
+namespace Benji.PSW.Windows
+{
+    class MainProgram
+    {
+        static void Main(string[ ] args)
+        {
+            try { Code.Hub.Wrapper(args); } catch (Exception q) { Console.WriteLine(q.ToString()); }
+        }
+    }
+}
