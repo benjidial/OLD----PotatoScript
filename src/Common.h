5ab402dccd2c0d97c67465bf465f107e5e5b9d91
Benji Dial <benji3.141@gmail.com>
2017-12-29 15:36:16 -0500

    Update Common.h

--- src/Common.h
+++ src/Common.h
@@ -5,9 +5,9 @@
 #include <stdio.h>
 extern int errno;
 
-// PotatoScript Alpha 0.6.0 Alpha Common.h
-// The PotatoScript programming language is made by Benji Dial and Warren Galloway.  It is licensed under the MIT license.
-// The official PotatoScript GitHub repository is available at <https://github.com/benjidial/PotatoScript>.
+// PotatoScript Classic Alpha 0.6.0 Alpha Common.h
+// The PotatoScript programming language is made by Benji Dial and Warren Galloway.  It is licensed under the Apache license.
+// The official PotatoScript Classic GitHub repository is available at <https://github.com/benjidial/PotatoScript-Classic>.
 
 enum asmBits { SIXTEENBIT, THIRTYTWOBIT, SIXTYFOURBIT };
 enum os { LINUX, MACOSX, WINDOWS };
@@ -15,13 +15,11 @@ enum asmSyntax { GAS, NASM };
 
 enum errorCode { NOTSUPPORTED = -1, PSCFNF = -2, ASMFNF = -3 };
 
-int comingSoon(char* message)
-{
+int comingSoon(char *message) {
   return anError(message, NOTSUPPORTED);
 }
 
-int anError(char* message, int exitCode)
-{
+int anError(char *message, int exitCode) {
   perror(message);
   fprintf(stderr, "C Error code: 0x%X  PS Error code: 0x%X\n", errno, exitCode);
   return exitCode;
